fe0262b93831236e35b82fbbacee971346af8405
Fahdilou_ouro <fahdiloufahd@gmail.com>
2025-05-19 17:46:32 +0200

    new ifa_meander works

--- utils/ifa_meander_project/meander_function.py
+++ utils/ifa_meander_project/meander_function.py
@@ -1,5 +1,6 @@
 import gmsh
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from utils.gmsh_function import *
 from rwg.rwg1 import *
@@ -8,7 +9,7 @@ from rwg.rwg3 import *
 from rwg.rwg4 import *
 from rwg.rwg5 import *
 
-def calculate_nPoints(fLow, fHigh, fC, min_points=28):
+def calculate_nPoints(fLow, fHigh, fC, min_points=5):
     step = (fHigh - fLow) / (min_points - 1)
     if (fC - fLow) % step == 0:
         return min_points
@@ -21,76 +22,133 @@ def calculate_nPoints(fLow, fHigh, fC, min_points=28):
                 return nPoints
             nPoints += 1
 
-'''def ifa_creation(a, b, wid, min_slot):
-    gap = 0
-    n = (a - gap) / (wid + min_slot)
-    m = int(np.floor(n))
+def ifa_creation_new(L, largeur, hauteur, width, L_short = 1 / 1000):
+    # — Initialisation
+    x0 = 0
+    y0 = hauteur / 2 - width / 2
+    hauteur = hauteur - width
 
-    # Recalcul du slot minimal pour s'ajuster exactement à la hauteur `a`
-    min_slot = ((a - gap) - m * wid) / m
+    # — Calcul du nombre de brins verticaux
+    # N = int(np.floor((largeur / min_slot - 1)))
+    N = int(np.floor((L - largeur) / hauteur))
+    print(f"Number of meanders {N}")
+    distance_meandre = (largeur - L_short) / N
+    print(f"distance meandres {distance_meandre}")
 
-    x_dip = []
-    y_dip = []
+    # longueur_meandre = (N + 1) * min_slot + N * hauteur
 
-    # Point de départ (en haut à gauche)
-    x_dip.append(gap)
-    y_dip.append(b / 2)
+    x = np.zeros(2 * N + 3)
+    y = np.zeros(2 * N + 3)
 
-    x_dip.append(min_slot)
-    y_dip.append(b / 2)
+    x[0] = x0
+    y[0] = y0
 
-    j = 2  # compteur de points
+    direction = -1
+    idx = 0
+    calcul_actuel_longueur = 0
+    horizontal = False
+    vertical = False
 
-    # Première moitié du zigzag (de gauche à droite)
-    for i in range(m):
-        if i % 2 == 0:
-            # Descente
-            x_dip.append(i * a / m + wid + min_slot)
-            y_dip.append(b / 2)
+    idx += 1
+    x[idx] = x[idx - 1] + L_short
+    y[idx] = y[idx - 1]
 
-            x_dip.append(i * a / m + wid + min_slot)
-            y_dip.append(-b / 2 + wid)
-        else:
-            # Montée
-            x_dip.append(i * a / m + min_slot)
-            y_dip.append(-b / 2 + wid)
+    for k in range(1, N + 1):
+        # Vertical
+        idx += 1
+        x[idx] = x[idx - 1]
+        y[idx] = y[idx - 1] + direction * hauteur
+        calcul_actuel_longueur += hauteur
+        direction = -direction
 
-            x_dip.append(i * a / m + min_slot)
-            y_dip.append(b / 2)
+        # Horizontal
+        idx += 1
+        x[idx] = x[idx - 1] + distance_meandre - width / 4
+        y[idx] = y[idx - 1]
+        # calcul_actuel_longueur += distance_meandre - width / 2
+    
+    print(f"last index = {idx}")
 
-    # Dernier point à droite
-    if m % 2 == 0:
-        x_dip.append(a)
-        y_dip.append(b / 2)
-    else:
-        x_dip.append(a)
-        y_dip.append(-b / 2)
+    # Horizontal
+    idx += 1
+    x[idx] = x[idx - 1] + distance_meandre - width / 4
+    y[idx] = y[idx - 1]
+    # calcul_actuel_longueur += distance_meandre - width / 2
 
-    # Deuxième moitié du zigzag (de droite à gauche)
-    for i in range(m):
-        if (m - i) % 2 == 1:
-            # Montée
-            x_dip.append(a - (i * a / m) - wid)
-            y_dip.append(-b / 2)
+    # Ajouter le dernier petit segment correctif
+    # idx += 1
+    x[idx] = x[idx - 1]
+    y[idx] = y[idx - 1] + direction * hauteur
+    calcul_actuel_longueur += distance_meandre
+    
+    print("\nlongueur_obtenue =", calcul_actuel_longueur)
+    print("longueur_desiree =", L, "\n")
 
-            x_dip.append(a - (i * a / m) - wid)
-            y_dip.append(b / 2 - wid)
-        else:
-            # Descente
-            x_dip.append(a - (i * a / m))
-            y_dip.append(b / 2 - wid)
+    return x[:idx+1], y[:idx+1], N
 
-            x_dip.append(a - (i * a / m))
-            y_dip.append(-b / 2)
+def ifa_creation_optimisation(L, largeur, hauteur, width, Nombre_meandre, L_short = 2):
+    # — Initialisation
+    x0 = 0
+    y0 = hauteur / 2 - width / 2
+    hauteur = hauteur - width
 
-    # Retour final à gauche
-    x_dip.append(gap)
-    y_dip.append(b / 2 - wid)
+    # — Calcul du nombre de brins verticaux
+    # N = int(np.floor((largeur / min_slot - 1)))
+    N = Nombre_meandre
+    print(f"Number of meanders {N}")
+    distance_meandre = (largeur - L_short) / N
+    print(f"distance meandres {distance_meandre}")
 
-    x = np.array(x_dip)
-    y = np.array(y_dip)
+    # longueur_meandre = (N + 1) * min_slot + N * hauteur
 
-    return x, y'''
+    x = np.zeros(2 * N + 3)
+    y = np.zeros(2 * N + 3)
+
+    x[0] = x0
+    y[0] = y0
+
+    direction = -1
+    idx = 0
+    calcul_actuel_longueur = 0
+    horizontal = False
+    vertical = False
+
+    idx += 1
+    x[idx] = x[idx - 1] + L_short
+    y[idx] = y[idx - 1]
+
+    for k in range(1, N + 1):
+        # Vertical
+        idx += 1
+        x[idx] = x[idx - 1]
+        y[idx] = y[idx - 1] + direction * hauteur
+        calcul_actuel_longueur += hauteur
+        direction = -direction
+
+        # Horizontal
+        idx += 1
+        x[idx] = x[idx - 1] + distance_meandre - width / 4
+        y[idx] = y[idx - 1]
+        # calcul_actuel_longueur += distance_meandre - width / 2
+    
+    print(f"last index = {idx}")
+
+    # Horizontal
+    idx += 1
+    x[idx] = x[idx - 1] + distance_meandre - width / 4
+    y[idx] = y[idx - 1]
+    # calcul_actuel_longueur += distance_meandre - width / 2
+
+    # Ajouter le dernier petit segment correctif
+    # idx += 1
+    x[idx] = x[idx - 1]
+    y[idx] = y[idx - 1] + direction * hauteur
+    calcul_actuel_longueur += distance_meandre
+    
+    print("\nlongueur_obtenue =", calcul_actuel_longueur)
+    print("longueur_desiree =", L, "\n")
+
+    return x[:idx+1], y[:idx+1]
 
 def ifa_creation(a, b, first_min_slot, other_min_slot, m_max=100):
     gap = 0
@@ -194,8 +252,8 @@ def ifa_creation_center(L, largeur, hauteur, width, min_slot):
     N = int(np.floor((largeur / min_slot - 1)))
     longueur_meandre = (N + 1) * min_slot + N * hauteur
 
-    '''print("longueur_meandre =", longueur_meandre)
-    print("longueur_desiree =", L, "\n")'''
+    print("longueur_meandre =", longueur_meandre)
+    print("longueur_desiree =", L, "\n")
 
     x = np.zeros(2 * N + 2)
     y = np.zeros(2 * N + 2)
@@ -334,6 +392,90 @@ def trace_meander(x, y, Width):
 
     return x_meander, y_meander
 
+def trace_meander_new(x, y, Width):
+    """
+    Génère le contour épais (meander) autour min_slot'une ligne polygonale donnée.
+    
+    Paramètres :
+        x : array-like, abscisses de la ligne centrale
+        y : array-like, ordonnées de la ligne centrale
+        Width : hauteur totale du contour (centré sur la ligne)
+        
+    Retourne :
+        x_meander, y_meander : coordonnées du contour
+    """
+    x = np.array(x)
+    y = np.array(y)
+    n = len(x)
+
+    x_meander = np.zeros(2 * n)
+    y_meander = np.zeros(2 * n)
+
+    # Premier point
+    if x[0] == x[1] and y[0] > y[1]:
+        x_meander[0]     = x[0] + Width / 2
+        x_meander[2*n-1] = x[0] - Width / 2
+        y_meander[0]     = y[0]
+        y_meander[2*n-1] = y[0]
+    elif x[0] == x[1] and y[0] < y[1]:
+        x_meander[0]     = x[0] - Width / 2
+        x_meander[2*n-1] = x[0] + Width / 2
+        y_meander[0]     = y[0]
+        y_meander[2*n-1] = y[0]
+    elif y[0] == y[1]:
+        x_meander[0]     = x[0]
+        x_meander[2*n-1] = x[0]
+        y_meander[0]     = y[0] + Width / 2
+        y_meander[2*n-1] = y[0] - Width / 2
+
+    # Dernier point
+    if y[n-2] == y[n-1]:
+        x_meander[n-1] = x[n-1]
+        x_meander[n]   = x[n-1]
+        y_meander[n-1] = y[n-1] + Width / 2
+        y_meander[n]   = y[n-1] - Width / 2
+    elif x[n-2] == x[n-1] and y[n-2] > y[n-1]:
+        x_meander[n-1] = x[n-1] + Width / 2
+        x_meander[n]   = x[n-1] - Width / 2
+        y_meander[n-1] = y[n-1] - Width / 2  # modif
+        y_meander[n]   = y[n-1] - Width / 2  # modif
+    elif x[n-2] == x[n-1] and y[n-2] < y[n-1]:
+        x_meander[n-1] = x[n-1] - Width / 2
+        x_meander[n]   = x[n-1] + Width / 2
+        y_meander[n-1] = y[n-1] + Width / 2  # modif
+        y_meander[n]   = y[n-1] + Width / 2  # modif
+
+    # Points intermédiaires
+    j = 2 * n - 2
+    for i in range(1, n - 1):
+        if y[i-1] == y[i] and x[i] == x[i+1] and y[i] > y[i+1]:
+            x_meander[i] = x[i] + Width / 2
+            y_meander[i] = y[i] + Width / 2
+            x_meander[j] = x[i] - Width / 2
+            y_meander[j] = y[i] - Width / 2
+
+        elif x[i-1] == x[i] and y[i] == y[i+1] and y[i-1] > y[i+1]:
+            x_meander[i] = x[i] + Width / 2
+            y_meander[i] = y[i] + Width / 2
+            x_meander[j] = x[i] - Width / 2
+            y_meander[j] = y[i] - Width / 2
+
+        elif y[i-1] == y[i] and x[i] == x[i+1] and y[i] < y[i+1]:
+            x_meander[i] = x[i] - Width / 2
+            y_meander[i] = y[i] + Width / 2
+            x_meander[j] = x[i] + Width / 2
+            y_meander[j] = y[i] - Width / 2
+
+        elif x[i-1] == x[i] and y[i] == y[i+1] and y[i-1] < y[i+1]:
+            x_meander[i] = x[i] - Width / 2
+            y_meander[i] = y[i] + Width / 2
+            x_meander[j] = x[i] + Width / 2
+            y_meander[j] = y[i] - Width / 2
+
+        j -= 1
+
+    return x_meander, y_meander
+
 def antenna_ifa_meander(meander_x, meander_y, terminal_x, terminal_y, feed_x, feed_y, save_mesh_folder, mesh_name, mesh_size):
     gmsh.initialize()
     model_name  = "IFA_meander"
@@ -360,7 +502,7 @@ def antenna_ifa_meander(meander_x, meander_y, terminal_x, terminal_y, feed_x, fe
     # Afficher le modèle dans l’interface Gmsh
     gmsh.model.mesh.generate(2)
 
-    run()
+    # run()
 
     write(save_mesh_folder, mesh_name)
 
@@ -443,7 +585,7 @@ def radiation_ifa(mesh1, frequency, feed_point, voltage_amplitude, show):
 
     return impedance, surface_current_density
 
-def simulate_freq_loop(fLow, fHigh, nPoints, fC, accuracy, ifa_meander_mat, feed_point):
+def simulate_freq_loop(fLow, fHigh, nPoints, fC, accuracy, ifa_meander_mat, feed_point, distance_short, wid, L, hauteur, largeur, L_short, Nombre_meandre):
     Z0 = 50  # Impédance caractéristique en ohms
     frequencies = np.linspace(fLow, fHigh, nPoints)
     s11_db = []
@@ -451,6 +593,11 @@ def simulate_freq_loop(fLow, fHigh, nPoints, fC, accuracy, ifa_meander_mat, feed
     has_converged = False
     count = 0
     show = False
+    impedances = []
+    sf_list = []
+    new_distance_short = distance_short
+    new_wid = wid
+    new_Nombre_meandre = Nombre_meandre
 
     for frequency in frequencies:
         print(f"Simulation Numéro {count + 1}")
@@ -461,6 +608,7 @@ def simulate_freq_loop(fLow, fHigh, nPoints, fC, accuracy, ifa_meander_mat, feed
         else:
             show = False
         impedance, _ = radiation_ifa(ifa_meander_mat, frequency, feed_point, voltage_amplitude, show)
+        impedances.append(impedance)
         s11 = (impedance - Z0) / (impedance + Z0)
         s11_db.append(20 * np.log10(abs(s11)))
         print(f"paramètre S11 = {s11_db[count]} db")
@@ -469,17 +617,47 @@ def simulate_freq_loop(fLow, fHigh, nPoints, fC, accuracy, ifa_meander_mat, feed
     # Trouver la fréquence de résonance (minimum S11)
     min_index = np.argmin(s11_db)
     f_resonance = frequencies[min_index]
+    R_I_min_index = impedances[min_index].real
+    print(f"R_I_min_index = {R_I_min_index}")
     print(f"\nFréquence de résonance : {f_resonance / 1e6:.2f} MHz")
+
+    sf_list.append(new_distance_short)
 
     # Comparaison à la fréquence de coupure
     error = abs((fC - f_resonance) / fC)
-    if error <= accuracy:
-        has_converged = True
-        print(f" Convergence atteinte : |f_res - fC| = {error:.2f} Hz ≤ {accuracy}")
+    s11_db_min_index = s11_db[min_index]
+    if error < accuracy:
+        if s11_db_min_index < -10:
+            has_converged = True
+            print(f" Convergence atteinte : |f_res - fC| = {error:.2f} Hz ≤ {accuracy}")
+        else:
+            print(f"R_I_min_index = {R_I_min_index} $\Omega$")
+            # new_distance_short = distance_short * pow((Z0 / R_I_min_index), 2) 
+            new_distance_short = distance_short * pow((Z0 / R_I_min_index), 2)
+            # new_distance_short = 7.77 / 1000
+            if new_distance_short < 0.5 / 1000:
+                new_distance_short = 0.5 / 1000
+            if new_distance_short > hauteur:
+                new_distance_short = hauteur - wid
     else:
+        distance_meandre = (largeur - L_short) / Nombre_meandre
+        if distance_meandre < 0.5 / 1000:
+            distance_meandre = 0.5 / 1000
+        # new_Nombre_meandre = np.floor((largeur - L_short) / (wid + distance_meandre))
+        # new_Nombre_meandre = int(np.floor(Nombre_meandre * pow((fC / f_resonance), 2)))
+        new_Nombre_meandre = int(math.ceil((L / hauteur) * f_resonance / fC)) + 1 
+        new_wid = wid * pow((fC / f_resonance), 2)
+        # new_distance_short = distance_short * (Z0 / R_I_min_index)
+        new_distance_short = distance_short * (Z0 / R_I_min_index)
+        print(f"\n0...........short feed ...... dans la fonction = {new_distance_short * 1000}\n")
+        if new_distance_short < 0.5 / 1000:
+            new_distance_short = 0.5 / 1000
+        if new_distance_short > hauteur:
+            new_distance_short = hauteur
         print(f" Pas de convergence : |f_res - fC| = {error:.2f} Hz > {accuracy}")
+        print(f"\n1...........short feed ...... dans la fonction = {new_distance_short * 1000}\n")
 
-    return s11_db, f_resonance, has_converged
+    return s11_db, f_resonance, new_distance_short, new_wid, new_Nombre_meandre, has_converged
 
 
 def plot_s11_curve(fLow, fHigh, nPoints, s11_db, fC=None):
